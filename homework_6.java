2b2aa87e140cfc520f8e1e32d83fd18c6b26d077
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-05-25 22:58:23 +0500

    Add files via upload

---
+++ homework_6.java
@@ -0,0 +1,120 @@
+package new_package;
+	
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Random;
+import java.util.TreeMap;
+
+public class homework_6 {
+
+	public static void main(String[] args) {
+		FakeSet set_1 = new FakeSet();
+		for (int i = 0; i < 10; i++) {
+			set_1.add(new Random().nextInt(20), set_1);
+		}
+		System.out.println(set_1.contains(5));
+		System.out.println(Arrays.toString(set_1.getAll()));
+		
+		Iterator<Integer> iter = set_1.iterator();
+		while (iter.hasNext()) {
+			System.out.print(iter.next() + ", ");
+		}
+		
+		System.out.println("\n" + set_1.getItem(3));
+		
+		FakeTreeSet tset_1 = new FakeTreeSet();
+		
+		for (int i = 0; i < 10; i++) {
+			tset_1.add(new Random().nextInt(20), set_1);
+		}
+		System.out.println(tset_1.contains(5));
+		System.out.println(Arrays.toString(tset_1.getAll()));
+		
+		Iterator<Integer> iter2 = tset_1.iterator();
+		
+		while (iter2.hasNext()) {
+			System.out.print(iter2.next() + ", ");
+		}
+		
+		System.out.println("\n" + tset_1.getItem(3));
+		
+		Comparator<Integer> comparator = new Comparator<Integer>() {
+			public int compare (Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		};
+		
+	}
+}
+
+
+class FakeSet {
+	
+	private LinkedHashMap<Integer, Object> map_1 = new LinkedHashMap<>();
+	
+	static final Object OBJ = new Object();
+	
+	public boolean add(int number, Object OBJ) {
+		return map_1.put(number, OBJ) == null;
+	}
+	
+	public boolean contains(int number) {
+		return map_1.containsKey(number);
+	}
+	
+	public Object[] getAll() {
+		return map_1.keySet().toArray();
+	}
+	
+	public Iterator<Integer> iterator() {
+		return map_1.keySet().iterator();
+	}
+	
+	public Integer getItem(int index) {
+		return (Integer) getAll()[index];
+	}
+	
+}
+
+class FakeTreeSet {
+	
+	private Comparator<Integer> comparator;
+	
+	public FakeTreeSet() {
+		 comparator = new Comparator<Integer>() {
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o2 - o1;
+			}
+		};
+	}
+	
+
+	
+	private TreeMap<Integer, Object> tmap_1 = new TreeMap<>();
+	
+	static final Object OBJ = new Object();
+	
+	public boolean add(int number, Object OBJ) {
+		return tmap_1.put(number, OBJ) == null;
+	}
+	
+	public boolean contains(int number) {
+		return tmap_1.containsKey(number);
+	}
+	
+	public Object[] getAll() {
+		return tmap_1.keySet().toArray();
+	}
+	
+	public Iterator<Integer> iterator() {
+		return tmap_1.keySet().iterator();
+	}
+	
+	public Integer getItem(int index) {
+		return (Integer) getAll()[index];
+	}
+	
+}
